5b609a124f26e2baa6028fe34f351797501b588f
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-02-27 19:23:38 -0300

    fix: change import from AbrirImagem

--- Interaface/MenuBar/MenuItem/AbrirImagem.java
+++ Interaface/MenuBar/MenuItem/AbrirImagem.java
@@ -1,7 +1,6 @@
 package Interaface.MenuBar.MenuItem;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JMenuItem;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import Interaface.GUI;
